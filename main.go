aaa79f8ae12b5c549331179dce96f83e2df599f9
Emirton Sousa <emirtonf@gmail.com>
2023-09-14 13:08:45 -0300

    feat: pointers on inputs

--- main.go
+++ main.go
@@ -13,11 +13,22 @@ func main() {
 	fmt.Printf("We have a total %v tickets and %v tickets remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
 
-	fmt.Scan(&userName)
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email:")
+	fmt.Scan(&email)
+
+	fmt.Println("How many tickets do you want?")
 	fmt.Scan(&userTickets)
 
-	fmt.Printf("The user %v bought %v tickets\n", userName, userTickets)
+	fmt.Printf("The user %v %v bought %v tickets, you will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 }
