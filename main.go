80537002db48519df1ef47e117909c977512867c
Emirton Sousa <emirtonf@gmail.com>
2023-09-14 12:32:01 -0300

    feat: data types

--- main.go
+++ main.go
@@ -7,7 +7,17 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = conferenceTickets
 
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T and confName is %T\n", conferenceTickets, remainingTickets, confName)
+
 	fmt.Printf("Welcome to the %v book application\n", confName)
 	fmt.Printf("We have a total %v tickets and %v tickets remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here")
+
+	var userName string
+	var userTickets int
+
+	userName = "Teste"
+	userTickets = 1
+
+	fmt.Printf("The user %v bought %v tickets\n", userName, userTickets)
 }
